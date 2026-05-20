7daed643f2f79f3a2f9840e15e8e53d382dc9998
sabiluu <sabilurosyada@gmail.com>
2026-05-20 11:44:10 +0700

    tambah router halaman

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 // Router / Entry Point Utama
-// Redirect ke halaman home (Landing Page) sementara
-header("Location: views/public/home.php");
-exit;
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+$routes = [
+    'home' => __DIR__ . '/views/public/home.php',
+    'cara_kerja' => __DIR__ . '/views/public/cara_kerja.php',
+    'penjualan' => __DIR__ . '/views/public/penjualan.php',
+    'detail_tiket' => __DIR__ . '/views/public/detail_tiket.php',
+    'sell_ticket' => __DIR__ . '/views/seller/sell_ticket.php',
+    'transaction' => __DIR__ . '/views/seller/transaction.php',
+];
+
+if (!array_key_exists($page, $routes)) {
+    http_response_code(404);
+    $page = 'home';
+}
+
+require $routes[$page];
